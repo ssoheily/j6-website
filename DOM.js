d56eee2e43b2dd97168a15755ba053d965eb49e4
ssoheily <sam.soheil55@gmail.com>
2021-08-12 19:20:20 +0200

    get date mit DOM

---
+++ DOM.js
@@ -0,0 +1,14 @@
+
+
+// let header = document.getElementById("header")
+
+// header.style.color =  'blue'
+
+// let content=document.getElementsByClassName("content")
+// content[1].style.fontSize = '25px'
+// console.log(content)
+
+let paragraphs = document.getElementsByTagName("p")
+// console.log(paragraphs)
+console.log(paragraphs[0].innerText)
+paragraphs[0].innerText="edited : paragraph edited by js"
